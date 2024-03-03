17a9a7801213d106d3081b2d6431cd9c8567bc80
Tony Xiao <tonyx.ca@gmail.com>
2024-03-03 12:37:44 -0800

    feat: move hubspot-provider into its own folder and split amppers out

--- verticals/vertical-crm/providers/hubspot-provider/index.ts
+++ verticals/vertical-crm/providers/hubspot-provider/index.ts
@@ -1,273 +1,11 @@
-import type {BaseRecord} from '@supaglue/vdk'
-import {LastUpdatedAtNextOffset, mapper, uniqBy, z, zCast} from '@supaglue/vdk'
+import type {BaseRecord, z} from '@supaglue/vdk'
+import {LastUpdatedAtNextOffset, uniqBy} from '@supaglue/vdk'
 import {LRUCache} from 'lru-cache'
 import * as RM from 'remeda'
-import type {Oas_crm_contacts, Oas_crm_owners} from '@opensdks/sdk-hubspot'
 import {initHubspotSDK, type HubspotSDK} from '@opensdks/sdk-hubspot'
-import type {CRMProvider} from '../router'
-import {commonModels} from '../router'
-
-export type SimplePublicObject =
-  Oas_crm_contacts['components']['schemas']['SimplePublicObject']
-export type Owner = Oas_crm_owners['components']['schemas']['PublicOwner']
-
-//   // In certain cases, Hubspot cannot determine the object type based on just the name for custom objects,
-//   // so we need to get the ID.
-//  const getObjectTypeIdFromNameOrId = async(nameOrId: string): Promise<string> => {
-//     // Standard objects can be referred by name no problem
-//     if (isStandardObjectType(nameOrId)) {
-//       return nameOrId;
-//     }
-//     if (this.#isAlreadyObjectTypeId(nameOrId)) {
-//       return nameOrId;
-//     }
-//     await this.maybeRefreshAccessToken();
-//     const schemas = await this.#client.crm.schemas.coreApi.getAll();
-//     const schemaId = schemas.results.find((schema) => schema.name === nameOrId || schema.objectTypeId === nameOrId)
-//       ?.objectTypeId;
-//     if (!schemaId) {
-//       throw new NotFoundError(`Could not find custom object schema with name or id ${nameOrId}`);
-//     }
-//     return schemaId;
-//   }
-
-export const HUBSPOT_STANDARD_OBJECTS = [
-  'company',
-  'contact',
-  'deal',
-  'line_item',
-  'product',
-  'ticket',
-  'quote',
-  'call',
-  'communication',
-  'email',
-  'meeting',
-  'note',
-  'postal_mail',
-  'task',
-] as const
-
-const HSBase = z.object({
-  id: z.string(),
-  properties: z
-    .object({
-      hs_object_id: z.string(),
-      createdate: z.string().optional(),
-      lastmodifieddate: z.string().optional(),
-    })
-    .passthrough(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  archived: z.boolean(),
-})
-const HSContact = z.object({
-  id: z.string(),
-  properties: z
-    .object({
-      hs_object_id: z.string(),
-      createdate: z.string().nullish(),
-      lastmodifieddate: z.string(),
-      // properties specific to contacts below...
-      email: z.string().nullish(),
-      firstname: z.string().nullish(),
-      lastname: z.string().nullish(),
-    })
-    .passthrough(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  archived: z.boolean(),
-})
-const HSDeal = z.object({
-  id: z.string(),
-  properties: z
-    .object({
-      hs_object_id: z.string(),
-      createdate: z.string().nullish(),
-      lastmodifieddate: z.string().nullish(),
-      // properties specific to opportunities below...
-      dealname: z.string().nullish(),
-      hubspot_owner_id: z.string().nullish(),
-      notes_last_updated: z.string().nullish(), // Assuming lastActivityAt is a string in HubSpot format
-      dealstage: z.string().nullish(),
-      pipeline: z.string().nullish(),
-      closedate: z.string().nullish(), // Assuming closeDate is a string in HubSpot format
-      description: z.string().nullish(),
-      amount: z.string().nullish(),
-      hs_is_closed_won: z.string().nullish(),
-      hs_is_closed: z.string().nullish(),
-
-      // account_id: z.string().nullish(),
-      // status: z.string().nullish(),
-      is_deleted: z.boolean().nullish(), // Does this exist?
-      archivedAt: z.string().nullish(), // Does this exist?
-    })
-    .passthrough(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  archived: z.boolean(),
-  /** toObjectType => toObjectId[] */
-  '#associations': z
-    .record(z.enum(['company']), z.array(z.string()))
-    .optional(),
-  '#pipelineStageMapping': zCast<PipelineStageMapping>(),
-})
-const HSAccount = z.object({
-  id: z.string(),
-  properties: z
-    .object({
-      hs_object_id: z.string(),
-      createdate: z.string().nullish(),
-      lastmodifieddate: z.string().nullish(),
-      name: z.string().nullish(),
-      description: z.string().nullish(),
-      hubspot_owner_id: z.string().nullish(),
-      industry: z.string().nullish(),
-      website: z.string().nullish(),
-      numberofemployees: z.string().nullish(),
-      addresses: z.string().nullish(), // Assuming addresses is a string; adjust the type if needed
-      phonenumbers: z.string().nullish(), // Assuming phonenumbers is a string; adjust the type if needed
-      lifecyclestage: z.string().nullish(),
-      notes_last_updated: z.string().nullish(),
-    })
-    .passthrough(),
-  createdAt: z.string(),
-  updatedAt: z.string(),
-  archived: z.boolean(),
-})
-
-const propertiesToFetch = {
-  account: [
-    'Id',
-    'Name',
-    'Type',
-    'ParentId',
-    'BillingAddress',
-    'ShippingAddress',
-    'Phone',
-    'Fax',
-    'Website',
-    'Industry',
-    'NumberOfEmployees',
-    'OwnerId',
-    'CreatedDate',
-    'LastModifiedDate',
-  ],
-  company: [
-    'hubspot_owner_id',
-    'description',
-    'industry',
-    'website',
-    'domain',
-    'hs_additional_domains',
-    'numberofemployees',
-    'address',
-    'address2',
-    'city',
-    'state',
-    'country',
-    'zip',
-    'phone',
-    'notes_last_updated',
-    'lifecyclestage',
-    'createddate',
-  ],
-  contact: [
-    'address', // TODO: IP state/zip/country?
-    'address2',
-    'city',
-    'country',
-    'email',
-    'fax',
-    'firstname',
-    'hs_createdate', // TODO: Use this or createdate?
-    'hs_is_contact', // TODO: distinguish from "visitor"?
-    'hubspot_owner_id',
-    'lifecyclestage',
-    'lastname',
-    'mobilephone',
-    'phone',
-    'state',
-    'work_email',
-    'zip',
-  ],
-  deal: [
-    'dealname',
-    'description',
-    'amount',
-    'hubspot_owner_id',
-    'notes_last_updated',
-    'closedate',
-    'dealstage',
-    'pipeline',
-    'hs_is_closed_won',
-    'hs_is_closed',
-  ],
-}
-
-const mappers = {
-  account: mapper(HSAccount, commonModels.account, {
-    id: 'id',
-    name: 'properties.name',
-    updated_at: (record) => new Date(record.updatedAt).toISOString(),
-    is_deleted: (record) => !!record.archived,
-    website: 'properties.website',
-    industry: 'properties.industry',
-    number_of_employees: (record) =>
-      record.properties.numberofemployees
-        ? Number.parseInt(record.properties.numberofemployees, 10)
-        : null,
-    owner_id: 'properties.hubspot_owner_id',
-    created_at: (record) => new Date(record.createdAt).toISOString(),
-  }),
-  contact: mapper(HSContact, commonModels.contact, {
-    id: 'id',
-    first_name: 'properties.firstname',
-    last_name: 'properties.lastname',
-    updated_at: (record) => new Date(record.updatedAt).toISOString(),
-  }),
-  opportunity: mapper(HSDeal, commonModels.opportunity, {
-    id: 'id',
-    name: 'properties.dealname',
-    description: 'properties.description',
-    owner_id: 'properties.hubspot_owner_id',
-    status: (record) =>
-      record.properties.hs_is_closed_won
-        ? 'WON'
-        : record.properties.hs_is_closed
-          ? 'LOST'
-          : 'OPEN',
-    stage: (r) =>
-      r['#pipelineStageMapping'][r.properties.pipeline ?? '']?.stageLabelById?.[
-        r.properties.dealstage ?? ''
-      ],
-    account_id: (r) => r['#associations']?.company?.[0],
-    close_date: 'properties.closedate',
-    amount: (record) =>
-      record.properties.amount
-        ? Number.parseFloat(record.properties.amount)
-        : null,
-    last_activity_at: 'properties.notes_last_updated',
-    created_at: 'properties.createdate',
-    // TODO: take into account archivedAt if needed
-    updated_at: (record) => new Date(record.updatedAt).toISOString(),
-    last_modified_at: (record) => new Date(record.updatedAt).toISOString(),
-  }),
-  lead: mapper(HSBase, commonModels.lead, {
-    id: 'id',
-    updated_at: (record) => new Date(record.updatedAt).toISOString(),
-  }),
-  user: mapper(zCast<Owner>(), commonModels.user, {
-    id: 'id',
-    updated_at: 'updatedAt',
-    created_at: 'createdAt',
-    last_modified_at: 'updatedAt',
-    name: (o) => [o.firstName, o.lastName].filter((n) => !!n?.trim()).join(' '),
-    email: 'email',
-    is_active: (record) => !record.archived, // Assuming archived is a boolean
-    is_deleted: (record) => !!record.archived, // Assuming archived is a boolean
-  }),
-}
+import type {CRMProvider} from '../../router'
+import type {HSDeal} from './mappers'
+import {HUBSPOT_STANDARD_OBJECTS, mappers, propertiesToFetch} from './mappers'
 
 const _listEntityIncrementalThenMap = async <TIn, TOut extends BaseRecord>(
   instance: HubspotSDK,
@@ -493,7 +231,9 @@ const cachedGetPipelineStageMapping = async (
   return res
 }
 
-type PipelineStageMapping = Awaited<ReturnType<typeof _getPipelineStageMapping>>
+export type PipelineStageMapping = Awaited<
+  ReturnType<typeof _getPipelineStageMapping>
+>
 const _getPipelineStageMapping = async (instance: HubspotSDK) => {
   const res = await instance.crm_pipelines.GET(
     '/crm/v3/pipelines/{objectType}',
@@ -593,6 +333,10 @@ export const hubspotProvider = {
       page_size: input?.page_size,
       cursor: input?.cursor,
     }),
+
+  // MARK: - Custom objects
+
+  // MARK: - Metadata endpoints
   metadataListObjects: async ({instance, input}) =>
     uniqBy(
       [
