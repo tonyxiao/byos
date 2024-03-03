cab4656c86b7fb57c19567f10d6dc5b2a7b51aae
Tony Xiao <tonyx.ca@gmail.com>
2024-03-02 19:17:59 -0800

    chore: Allow 5 mins for inngest function for sync

--- main/app/api/inngest/route.ts
+++ main/app/api/inngest/route.ts
@@ -1,5 +1,9 @@
 import {functions, inngest, nextServe} from '@supaglue/worker'
 
+// https://vercel.com/docs/functions/configuring-functions/duration
+// https://vercel.com/docs/functions/runtimes#max-duration
+export const maxDuration = 300 // Pro lan maximum, todo: make me configurable via env var
+
 export const {GET, POST, PUT} = nextServe({
   client: inngest,
   functions: Object.values(functions),
