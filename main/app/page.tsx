46178bff02d92db48757ad0f4387689aba80ea29
Tony Xiao <tonyx.ca@gmail.com>
2024-03-05 02:59:14 -0800

    chore: remove not-ready home page

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -1,53 +1,48 @@
-import {initNangoSDK} from '@opensdks/sdk-nango'
-import {initSupaglueSDK} from '@opensdks/sdk-supaglue'
-import {env} from '@/env'
-import {NewConnection} from './NewConnection'
-
-async function listConnections() {
-  // if (env.SUPAGLUE_API_KEY && featureFlags.mode === 'supaglue') {
-  //   const supaglue = initSupaglueSDK({
-  //     headers: {'x-api-key': env.SUPAGLUE_API_KEY},
-  //   })
-  //   const cusRes = await supaglue.mgmt.GET('/customers')
-  //   const connections = await Promise.all(
-  //     cusRes.data.map((c) =>
-  //       supaglue.mgmt.GET('/customers/{customer_id}/connections', {
-  //         params: {path: {customer_id: c.customer_id}},
-  //       }),
-  //     ),
-  //   ).then((resArray) => resArray.flatMap((res) => res.data))
-  //   return connections.map((c) => ({
-  //     id: c.id,
-  //     connection_id: `customerId: ${c.customer_id}\n connectionId: ${c.id}`,
-  //     provider: c.provider_name,
-  //     // customer_id: c.customer_id,
-  //   }))
-  // }
-  // if (env.NANGO_SECRET_KEY && featureFlags.mode === 'nango') {
-  const nango = initNangoSDK({
-    headers: {authorization: `Bearer ${env.NANGO_SECRET_KEY}`},
-  })
-  const res = await nango.GET('/connection')
-
-  return (
-    res.data.configs?.map((c) => ({
-      id: c.id,
-      provider: c.provider,
-      connection_id: c.connection_id,
-      // CustomerId does not exist in nango...
-    })) || []
-  )
-  // }
-  // throw new Error('Neither Supaglue nor nango is initialized')
-}
+// async function listConnections() {
+// if (env.SUPAGLUE_API_KEY && featureFlags.mode === 'supaglue') {
+//   const supaglue = initSupaglueSDK({
+//     headers: {'x-api-key': env.SUPAGLUE_API_KEY},
+//   })
+//   const cusRes = await supaglue.mgmt.GET('/customers')
+//   const connections = await Promise.all(
+//     cusRes.data.map((c) =>
+//       supaglue.mgmt.GET('/customers/{customer_id}/connections', {
+//         params: {path: {customer_id: c.customer_id}},
+//       }),
+//     ),
+//   ).then((resArray) => resArray.flatMap((res) => res.data))
+//   return connections.map((c) => ({
+//     id: c.id,
+//     connection_id: `customerId: ${c.customer_id}\n connectionId: ${c.id}`,
+//     provider: c.provider_name,
+//     // customer_id: c.customer_id,
+//   }))
+// }
+// if (env.NANGO_SECRET_KEY && featureFlags.mode === 'nango') {
+// const nango = initNangoSDK({
+//   headers: {authorization: `Bearer ${env.NANGO_SECRET_KEY}`},
+// })
+// const res = await nango.GET('/connection')
+// return (
+//   res.data.configs?.map((c) => ({
+//     id: c.id,
+//     provider: c.provider,
+//     connection_id: c.connection_id,
+//     // CustomerId does not exist in nango...
+//   })) || []
+// )
+// }
+// throw new Error('Neither Supaglue nor nango is initialized')
+// }
 
 export default async function Home() {
-  const connections = await listConnections()
+  // const connections = await listConnections()
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Current Connections</h1>
-      <table>
+      <h1 className="text-4xl font-bold mb-8">API Only. Management UI WIP</h1>
+
+      {/* <table>
         <thead>
           <tr>
             <th>Provider Config Key</th>
@@ -64,7 +59,7 @@ export default async function Home() {
         </tbody>
       </table>
       <h1 className="text-4xl font-bold mb-8">New connection</h1>
-      <NewConnection />
+      <NewConnection /> */}
     </main>
   )
 }
